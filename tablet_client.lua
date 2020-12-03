043759ec908c21691b6578cca7b49c962cec748f
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-12-03 17:26:19 -0500

    Update tablet_client.lua

--- tablet_client.lua
+++ tablet_client.lua
@@ -1,6 +1,10 @@
 local component = require("component")
 local event = require("event")
 
+local m = component.modem -- get primary modem component
+m.open(123)
+print(m.isOpen(123))
+
 function disp(cur,max,alive)
   gpu.setResolution(23,5)
   gpu.setBackground(0x000000,false)
@@ -13,6 +17,6 @@ function disp(cur,max,alive)
   gpu.fill(1,3,w,1, " ")
 end
 while true do
-  local _, _, from, port, _, message = event.pull("modem_message")
-  print("Got a message from " .. from .. " on port " .. port .. ": " .. tostring(message))
+  local test, _, _, _, _, message = event.pull("modem_message")
+  print(test)
 end
